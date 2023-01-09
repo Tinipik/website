fd921b610210b76e70d34bbadaca9320c85f6918
Tinge <qpicault@yahoo.com>
2023-01-09 18:25:05 +0100

    Sitemap gen

---
+++ pages/sitemap.xml.tsx
@@ -0,0 +1,27 @@
+import { GetServerSideProps } from "next";
+
+const generateSiteMap = () => {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <url>
+       <loc>https://quentinpicault.fr</loc>
+     </url>
+   </urlset>
+ `;
+}
+
+const SiteMap = () => {}
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const sitemap = generateSiteMap();
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
